d01a7ae5650ed461ea8afae066d8c4e5776ccdf2
donuhead7 <kennethdu3@gmail.com>
2019-05-15 15:24:25 -0700

    init commit of r/pics remake

---
+++ src/components/RedditList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Reddit clone /r/pics</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-list>
+      <ion-item v-for="post in posts" :key="post.data.id" @click="handleClick(post.data.preview.images[0].source.url)">
+        <img :src="post.data.thumbnail" alt="thumb" class="thumbnail">
+        <ion-label>{{ post.data.title }}</ion-label>
+      </ion-item>
+    </ion-list>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'RedditList',
+  props: {
+    msg: String
+  },
+  mounted() {
+    axios.get('https://www.reddit.com/r/pics.json?raw_json=1')
+      .then(response => {
+        // console.log(response.data.data.children);
+        this.posts = response.data.data.children
+      })
+  },
+  data() {
+    return {
+      posts: [],
+      myInput: ''
+    }
+  },
+  methods: {
+    handleClick(imageSrc) {
+      this.$router.push({ name: 'about', params: { imageSrc }})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .thumbnail {
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+    margin-right: 16px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <RedditList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RedditList from '@/components/RedditList.vue'
+
+export default {
+  name: 'home',
+  components: {
+    RedditList
+  }
+}
+</script>
